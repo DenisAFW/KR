f48c3943b598ab90a3c81717e869dcfae369f85c
Choppa <denis.afinogenow@yandex.ru>
2022-08-07 16:00:13 +0300

    Написал код по задаче

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] words(string[] array)
+{
+
+    string[] NewWords = new string[11];
+
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            NewWords[count] = array[i];
+            count++;
+        }
+    }
+    return NewWords;
+}
+
+void Print(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] NewArray = { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan" };
+
+Print(words(NewArray));
